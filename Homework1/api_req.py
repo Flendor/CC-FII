06400eb7bec81bfe30b38dba311ec2a724434a97
Matei <matei_alex1998@yahoo.ro>
2020-02-25 11:14:01 -0800

    Wrote logs in json files

--- Homework1/api_req.py
+++ Homework1/api_req.py
@@ -1,34 +1,52 @@
 from initializer import init_data
 import requests
+import time
+from statistics import log_deezer, log_random, log_songsterr
 
 
 def request_to_deezer(artist, url, header_name, header_value):
+    start_time = time.time()
     try:
         songs = requests.get(url + artist, headers={header_name: header_value})
         songs_json = songs.json()
         return songs_json
-    except:
+    except Exception as e:
+        songs_json = "Request Failed"
         print("Deezer request failed!")
+        print(e)
+    finally:
+        final_time = (time.time() - start_time) * 1000
+        log_deezer(url, artist, songs_json, final_time, songs.status_code)
 
 
 def request_to_random(url, max_random_value, header_name, header_value):
+    start_time = time.time()
     try:
         number = requests.get(url + max_random_value, headers={header_name: header_value})
         number_json = number.json()
         return number_json
-    except:
+    except Exception as e:
+        number_json = "Request Failed"
         print("NumbersApi request failed!")
+        print(e)
     finally:
-
+        final_time = (time.time() - start_time) * 1000
+        log_random(url, max_random_value, number_json, final_time, number.status_code)
 
 
 def request_to_songsterr(url, song_title):
+    start_time = time.time()
     try:
         songs_from_songsterr = requests.get(url + song_title)
         songs_from_songsterr_json = songs_from_songsterr.json()
         return songs_from_songsterr_json
-    except:
+    except Exception as e:
+        songs_from_songsterr_json = "Request Failed"
         print("Songsterr request failed!")
+        print(e)
+    finally:
+        final_time = (time.time() - start_time) * 1000
+        log_songsterr(url, song_title, songs_from_songsterr_json, final_time, songs_from_songsterr.status_code)
 
 
 class RequestController:
@@ -48,12 +66,14 @@ class RequestController:
             songsterr_json = request_to_songsterr(self.data[0][2], songs_json['data'][song_index]['title_short'])
             tab_id = None
             for elem in songsterr_json:
-                if elem['artist']['name'].lower() == artist.lower():
+                if elem['artist']['name'].lower() == artist.lower() and \
+                        elem['title'] == songs_json['data'][song_index]['title_short']:
                     tab_id = elem['id']
             if tab_id is None:
                 result_2 = None
             else:
                 result_2 = self.data[1][0] + str(tab_id)
             return result_1, result_2
-        except:
+        except Exception as e:
             print("Requests failed")
+            print(e)
--- Homework1/statistics.py
+++ Homework1/statistics.py
@@ -1,6 +1,46 @@
 import json
 
 
+def form_new_log(url, param, response, latency, status_code):
+    new_log = dict()
+    new_log['url'] = url + param
+    new_log['argument'] = param
+    new_log['response'] = response
+    new_log['latency'] = str(latency) + ' ms'
+    new_log['status_code'] = status_code
+    return new_log
+
+
+def log_deezer(url, param, response, latency, status_code):
+    new_log = form_new_log(url, param, response, latency, status_code)
+    with open("logs/deezer.json", 'r') as ddr:
+        deezer_json = json.load(ddr)
+        temp = deezer_json['calls']
+        temp.append(new_log)
+    with open("logs/deezer.json", 'w', encoding='utf-8') as ddw:
+        json.dump(deezer_json, ddw, ensure_ascii=False, indent=4)
+
+
+def log_random(url, param, response, latency, status_code):
+    new_log = form_new_log(url, param, response, latency, status_code)
+    with open("logs/random.json", 'r') as rdr:
+        random_json = json.load(rdr)
+        temp = random_json['calls']
+        temp.append(new_log)
+    with open("logs/random.json", 'w', encoding='utf-8') as rdw:
+        json.dump(random_json, rdw, ensure_ascii=False, indent=4)
+
+
+def log_songsterr(url, param, response, latency, status_code):
+    new_log = form_new_log(url, param, response, latency, status_code)
+    with open("logs/songsterr.json", 'r') as sdr:
+        songsterr_json = json.load(sdr)
+        temp = songsterr_json['calls']
+        temp.append(new_log)
+    with open("logs/songsterr.json", 'w', encoding='utf-8') as sdw:
+        json.dump(songsterr_json, sdw, ensure_ascii=False, indent=4)
+
+
 def compute_metrics():
     metrics = dict()
     metrics['result'] = "Momentan nu am implementat frate"
